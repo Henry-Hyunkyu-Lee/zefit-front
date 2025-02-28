6d09a056fba1c27b725cdba5511aef2415a8b3fa
Henry-Hyunkyu-Lee <89893141+Henry-Hyunkyu-Lee@users.noreply.github.com>
2025-02-28 18:29:53 +0900

    Add files via upload

--- src/utils/MetaTagTitle.tsx
+++ src/utils/MetaTagTitle.tsx
@@ -1,14 +1,20 @@
 interface MetaTagTitleProps {
   title: string;
+  description?: string;
+  keywords?: string[];
   ko?: boolean;
 };
 
-export default function MetaTagTitle({ title, ko = true }: MetaTagTitleProps) {
+export default function MetaTagTitle({ title, description, keywords, ko = true }: MetaTagTitleProps) {
   return (
+  <>
     <title>
       {(title?.length > 0)
         ? `${title} | ${(ko) ? '제핏' : 'zefit'}`
         : `${(ko) ? '제핏' : 'zefit'}`}
     </title>
-  )
+	<meta name="description" content={description || "Default description"} />
+    <meta name="keywords" content={keywords?.join(', ') || "default,keywords"} />
+  </>
+  );
 };
