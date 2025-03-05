e68e00ce9ffc22f33fbe6757f9fb3a1e8580154f
Henry-Hyunkyu-Lee <89893141+Henry-Hyunkyu-Lee@users.noreply.github.com>
2025-03-05 18:03:26 +0900

    Add files via upload
    rollback

--- src/app/content/zebrafish/page.tsx
+++ src/app/content/zebrafish/page.tsx
@@ -1,33 +1,27 @@
 'use client';
 
 import PageHeader from '@/components/common/PageHeader';
-import '../../../content/zebrafish/style.css';
+import './style.css';
 import PageBanner from '@/components/common/PageBanner';
 import PageTap from '@/components/common/PageTap';
 import MetaTagTitle from '@/utils/MetaTagTitle';
-import { useMediaQuery } from 'react-responsive';
 import { isLoading } from '@/modules/loading';
 import { useRecoilState } from 'recoil';
 import { useEffect } from 'react';
 
-export default function ZebrafishModelEN() {
+export default function ZebrafishModel() {
 
     const [, setLoading] = useRecoilState(isLoading);
-
-    const isMobile = useMediaQuery({ maxWidth: 1170 });
 
     useEffect(() => {
         setLoading(false);
     }, []);
-
+    
     return (
         <article>
-            <MetaTagTitle title='Zebrafish Models for Drug Development and Research' ko={false}
-            description='ZEFIT offers zebrafish models for drug development and contract research. It provides insights into disease mechanisms, drug toxicity, and therapeutic efficacy.'
-            keywords={['zebrafish models','zebrafish contract research','zebrafish drug development','disease model',]}
-            />            
+            <MetaTagTitle title='제브라피쉬' />
             <PageHeader />
-            <PageBanner pageTitle='Zebrafish' />
+            <PageBanner pageTitle='제브라피쉬' />
             <PageTap tap='business' />
             <div className='page_layout'>
                 <section className='model_page_first_container'>
@@ -36,14 +30,14 @@ export default function ZebrafishModelEN() {
                     </h2>
                     <div className='model_page_top_info_wrapper'>
                         <h3 className='model_page_top_info_title'>
-                            {'Great advantages in securing treatment strategies for each disease'}
+                            {'질병 별 치료전략\n확보에 큰 강점'}
                         </h3>
                         <div className='top_info_content_wrapper'>
                             <p className='top_info_content'>
-                                {'Zebrafish is drug development strategies that have great advantages in securing a disease treatment strategy. Evolutionarily conserved genes are expected to be essential for sustaining life.'}
+                                {'제브라피쉬는 질병 치료전략 확보에 큰 강점을 가지는 신약개발 전략입니다.\n진화적으로 잘 보존된 유전자일 수록 생명유지에 필수적인 유전자일 가능성이 높습니다.'}
                             </p>
                             <p className='top_info_content'>
-                                {'Disease research utilizing Zebrafish model has great advantages in identifying disease’s core targets and obtaining proof of concept data for treatment strategies.'}
+                                {'제브라피쉬 모델에서의 질병연구는 질병 연관 핵심 타겟 규명 및 치료전략\nProof Of Concept 데이터 확보에 큰 강점을 가지고 있습니다.'}
                             </p>
                         </div>
                     </div>
@@ -51,18 +45,11 @@ export default function ZebrafishModelEN() {
                 <section className='model_page_second_container'>
                     <div className='model_intro_wrapper'>
                         <div className='model_intro_box'>
-                            <h3 
-                                style={{ fontSize: (isMobile) ? '16px' : '26px' }}
-                                className='model_intro_title'>
-                                Zebrafish pre-clinical data use cases
+                            <h3 className='model_intro_title'>
+                                제브라피쉬 도입 사례
                             </h3>
-                            <p
-                                style={{ fontSize: (isMobile) ? '13px' : '18px' }}
-                                className='model_intro_content'>
-                                Research at individual and organ level become best strategy to address bottlenecks in drug R&D
-                                According to the FDA report, global pharmaceutical companies, Novartis and Pfizer are reported using Zebrafish to reduce R&D costs by more than 10 million dollars per year.
-                                In addition, FDA IND application were reported as only zebrafish research data in 2019, 2020 and 2021.
-                                Zebrafish is emerging as a representative test model that can replace existing experimental animals.
+                            <p className='model_intro_content'>
+                                개체 및 기관 수준에서의 연구는 치료제 연구개발의 병목구간을 해결하는 최고의 전략이 됩니다. FDA 보고서에 따르면 글로벌 제약회사인 노바티스, 화이자는 제브라피쉬를 활용해 연간 1000억원 이상의 R&D 비용을 절감하는 것으로 보고되었습니다. 또한 2019, 2020년에는 제브라피쉬 단독 연구 데이터로 FDA의 임상계획승인이 보고되어 제브라피쉬는 기존의 실험동물을 대체할 수 있는 대표적인 시험모델로 떠오르고 있습니다.
                             </p>
                         </div>
                     </div>
@@ -70,10 +57,10 @@ export default function ZebrafishModelEN() {
                 <section className='model_page_third_container'>
                     <div className='model_development_intro_wrapper'>
                         <h3 className='model_development_intro_title'>
-                            {'Development of Zebrafish Model Utilization Research'}
+                            {'제브라피쉬 모델\n활용 연구의 발전'}
                         </h3>
                         <p className='model_development_intro_content'>
-                            {'Zebrafish was a model with large variations due to limitations in drug delivery methods, etc. at the early stages of drug research. However, with development of various drug delivery methods recently such as oral administration and intravenous injection, more sophisticated drug research is possible.'}
+                            {'제브라피쉬 모델은 약물연구 도입 초기 약물 전달 방법의 한계 등으로 인한 실험 편차가 큰 모델이였습니다.\n그러나 최근 구강투여, 정맥주사 등 다양한 약물전달 방법이 개발 됨에 따라 더욱 정교한 약물 연구가 가능합니다.'}
                         </p>
                     </div>
                 </section>
