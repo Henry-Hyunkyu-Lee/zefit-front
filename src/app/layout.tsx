1c228d434a40596a5ec0ce6fb7d22377039f9275
Henry-Hyunkyu-Lee <89893141+Henry-Hyunkyu-Lee@users.noreply.github.com>
2025-03-05 18:21:44 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import { headers } from "next/headers";
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "../styles/globals.css";
 import ClientProvider from "./clientProvider";
 import dynamic from "next/dynamic";
-
+import Script from "next/script"; // Import the Script component
 const notoSansKr = Noto_Sans_KR({
   // preload: true, 기본값
   subsets: ["latin"], // 또는 preload: false
@@ -24,9 +25,11 @@ const Loading = dynamic(() => import('../components/common/LoadingSpinner'), { s
 const Footer = dynamic(() => import('../components/common/Footer'), { ssr: false });
 
 export const metadata: Metadata = {
-  title: "제핏",
-  description: "Using zebrafish, in-vivo model based biotech & pharmaceutical company",
   icons: { icon: "/zefit.png", apple: "/zefit.png" },
+  verification: {
+    google:'5BqvjsTo5B9TZHzhKfNyLJHIr3v779_th9rvyEphT28',
+    bing:'A36ED9661F6899B09BE7549CED6FFC9C',
+         },
 };
 
 export default function RootLayout({
@@ -34,6 +37,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  //Dynamically set the canonical Tag
+  //created the middleware.ts file 
+  const headersList = headers();
+  const currentUrl = headersList.get("x-url") || headersList.get("referer") || "";
 
   return (
     <html lang="ko">
@@ -42,7 +49,23 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="canonical" href={currentUrl} />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        <meta name="robots" content="index,follow" />
+        <meta property="og:site_name" content="Zefit"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="https://ifvlnreaxggdzpirozcu.supabase.co/storage/v1/object/public/zefit_public/static_main_img1.jpg" />
+        <meta property="og:url" content={currentUrl} />
+         {/* Add Google Analytics script */}
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-FJBYH5L81Y"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FJBYH5L81Y');
+          `}
+        </Script>
       </head>
       <body className={notoSansKr.className}>
         <ClientProvider>
