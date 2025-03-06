66577a39108f62b8fc920c495fa111b959c434e7
Henry-Hyunkyu-Lee <89893141+Henry-Hyunkyu-Lee@users.noreply.github.com>
2025-03-06 11:49:51 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,10 +26,7 @@ const Footer = dynamic(() => import('../components/common/Footer'), { ssr: false
 
 export const metadata: Metadata = {
   icons: { icon: "/zefit.png", apple: "/zefit.png" },
-  verification: {
-    google:'5BqvjsTo5B9TZHzhKfNyLJHIr3v779_th9rvyEphT28',
-    bing:'A36ED9661F6899B09BE7549CED6FFC9C',
-         },
+  
 };
 
 export default function RootLayout({
@@ -52,6 +49,8 @@ export default function RootLayout({
         <link rel="canonical" href={currentUrl} />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
         <meta name="robots" content="index,follow" />
+        <meta name="msvalidate.01" content="A36ED9661F6899B09BE7549CED6FFC9C" />
+        <meta name="google-site-verification" content="5BqvjsTo5B9TZHzhKfNyLJHIr3v779_th9rvyEphT28" />
         <meta property="og:site_name" content="Zefit"/>
         <meta property="og:type" content="website"/>
         <meta property="og:image" content="https://ifvlnreaxggdzpirozcu.supabase.co/storage/v1/object/public/zefit_public/static_main_img1.jpg" />
