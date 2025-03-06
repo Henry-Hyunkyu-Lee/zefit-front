66da64581037ad05fa86e6883455642569092abb
Henry-Hyunkyu-Lee <89893141+Henry-Hyunkyu-Lee@users.noreply.github.com>
2025-03-06 15:01:38 +0900

    Update MetaTagTitle.tsx

--- src/utils/MetaTagTitle.tsx
+++ src/utils/MetaTagTitle.tsx
@@ -1,14 +1,21 @@
 interface MetaTagTitleProps {
   title: string;
   ko?: boolean;
+  description: string,
+  keywords: string[]
 };
 
-export default function MetaTagTitle({ title, ko = true }: MetaTagTitleProps) {
+export default function MetaTagTitle({ title, description, keywords, ko = true }: MetaTagTitleProps) {
   return (
     <title>
       {(title?.length > 0)
         ? `${title} | ${(ko) ? '제핏' : 'ZEFIT'}`
         : `${(ko) ? '제핏' : 'ZEFIT'}`}
     </title>
+      <meta name="description" content={description || ""} />
+      <meta name="keywords" content={keywords?.join(', ') || ""} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      </>
   )
 };
